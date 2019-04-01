b908bbae977609050ec5a3e61d023e4c1179157d
Arup Das <32811517+Arup276@users.noreply.github.com>
2019-04-01 23:36:19 +0530

    Add files via upload

---
+++ 65_Day/long_sum.py
@@ -0,0 +1,22 @@
+def lone_sum(a, b, c):
+
+	if a == b and b == c:
+    
+		return 0
+  
+	if a == b:
+    
+		return c
+  
+	if a == c:
+    
+		return b
+  
+	if b == c:
+    
+		return a
+  
+	else:
+    
+		return a+b+c
+    
---
+++ 65_Day/make_brick.py
@@ -0,0 +1,13 @@
+def make_bricks(small, big, goal):
+
+ 	sums = small + 5*big
+ 
+	req_small = goal % 5
+  
+	if sums >= goal and small >= req_small:
+    
+		return True
+  
+	else:
+	
+	return False
